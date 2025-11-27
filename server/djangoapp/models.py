435d036c2d9a4870786cb31ad31297e8834a72e2
Johm Prieto <johmprieto@gmail.com>
2025-11-26 22:55:40 -0500

    Initial project save

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,22 +1,18 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
 from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-
-# Create your models here.
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField()
 
     def __str__(self):
         return self.name
 
 class CarModel(models.Model):
-
-    # Limited car type options
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
@@ -25,20 +21,14 @@ class CarModel(models.Model):
         ('HATCHBACK', 'Hatchback'),
     ]
 
-    # Relationship: Many models belong to one make
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')
-
-    name = models.CharField(max_length=100)
-    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SEDAN')
-
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(
+        default=2023,
         validators=[
-            MinValueValidator(2015),
-            MaxValueValidator(2023)
-        ],
-        default=now().year
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
     )
 
     def __str__(self):
-        return f"{self.name} ({self.type}) - {self.year}"
-
+        return self.name
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,12 +31,14 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'johmprieto-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'johmprieto-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'johmprieto-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
-    'https://johmprieto-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://johmprieto-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://johmprieto-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 REST_FRAMEWORK = {
